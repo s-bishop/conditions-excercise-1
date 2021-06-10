41e5e09a9375343b19fba1c26544d1a393d63ab9
scottbishop <sa_bishop@comcast.net>
2021-06-10 15:25:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let actualTemp = 68;
+let desiredTemp = 68;
+if (actualTemp > desiredTemp) {
+  console.log("Run A/C");
+} else if (actualTemp == desiredTemp) {
+  console.log("Stand By");
+} else {
+  console.log("Run Heat");
+}
